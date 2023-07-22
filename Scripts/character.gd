c5213f0d7e2e9c94a97b11ec981ab2a650375d03
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2023-07-22 18:46:24 +0200

    player can hook to rock

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -4,6 +4,7 @@ class_name Player
 @export var SPEED = 3000
 @onready var fsm: FSM = $FSM
 @onready var pivot = $Pivot
+var hook_position = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,22 +13,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if fsm.state == AnimState.ROPE_PULL:
+		hook.global_position = hook_position
 
 func _physics_process(delta):
-	var movement = Vector2(Input.get_axis("move_left", "move_right"), \
-		Input.get_axis("move_up", "move_down")).normalized()
-	apply_central_force(SPEED * movement)
 	
-	# flip character
-	if movement.x != 0.0:
-		pivot.scale.x = 1.0 if movement.x >= 0.0 else -1.0
-		
-	# set correct state
-	if movement.length_squared() == 0.0:
-		fsm.set_state(AnimState.IDLE)
-	else:
-		fsm.set_state(AnimState.RUNNING)
+	if not $Rope.visible and fsm.state != AnimState.HUNG:
+		var movement = Vector2(Input.get_axis("move_left", "move_right"), \
+			Input.get_axis("move_up", "move_down")).normalized()
+		apply_central_force(SPEED * movement)
+	
+		# flip character
+		if movement.x != 0.0:
+			pivot.scale.x = 1.0 if movement.x >= 0.0 else -1.0
+			
+		if movement.length_squared() == 0.0:
+			fsm.set_state(AnimState.IDLE)
+		else:
+			fsm.set_state(AnimState.RUNNING)
 	
 	if Input.is_action_pressed("rope"):
 		hook_force = hook_rotate_force * Vector2.ZERO.direction_to(hook.position).rotated(-0.5 * PI)
@@ -36,22 +39,36 @@ func _physics_process(delta):
 
 var hook_force = Vector2.ZERO
 
-func _draw():
-	draw_line(hook.position, hook.position + 100.0 * Vector2.ZERO.direction_to(hook.position).rotated(-0.5 * PI), Color.RED, 10.0)
-
 func _input(event):
 	if Input.is_action_just_pressed("rope"):
+		hook.freeze = false
 		fsm.set_state(AnimState.ROPE_LAUNCH)
 	
 	if Input.is_action_just_released("rope"):
-		fsm.set_state(AnimState.ROPE_PULL)
-	
+		if hook.can_hook:
+			$CollisionShape2D.disabled = true
+			fsm.set_state(AnimState.ROPE_PULL)
+			var tween = create_tween()
+			tween.tween_property(self, "global_position", $Rope/Hook/HookPoint.global_position, 0.5)
+			tween.set_ease(Tween.EASE_IN_OUT)
+			tween.set_trans(Tween.TRANS_CUBIC)
+			tween.play()
+			hook.freeze = true
+			player.play("swing_to_hook")
+			hook_position = hook.global_position
+		else:
+			$Rope.hide()
+			fsm.set_state(AnimState.HUNG)
+
+func _on_end_swing():
+	fsm.set_state(AnimState.HUNG)
 
 enum AnimState {
 	IDLE,
 	RUNNING,
 	ROPE_LAUNCH,
 	ROPE_PULL,
+	HUNG,
 }
 
 @onready var player = $Pivot/AnimationPlayer
@@ -66,7 +83,8 @@ func _on_fsm_state_changed(old_state, new_state):
 			player.play("run")
 		AnimState.ROPE_LAUNCH:
 			$Rope.show()
-			
 		AnimState.ROPE_PULL:
-			$Rope.hide()
-			hook.add_constant_central_force(Vector2.ZERO)
+			pass
+		AnimState.HUNG:
+			player.play("hung")
+			print("HUNG state")
