bdffdf92e964dd011de0d7cfa634c599ca0fab7f
Pietro Lorefice <pietro.lorefice@gmail.com>
2023-07-23 11:17:54 +0200

    item: allow setting sprite scale

--- Scripts/pick_up_item.gd
+++ Scripts/pick_up_item.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name PickUpItem
 
 @export var item: Item : set = _set_item
-
+@export var sprite_scale: Vector2 = Vector2.ONE : set = _set_sprite_scale
 
 func _ready():
 	$InteractableComponent.interacted.connect(on_interacted)
@@ -11,6 +11,11 @@ func _ready():
 func _set_item(value: Item):
 	item = value
 	$Sprite2D.texture = item.texture
+
+
+func _set_sprite_scale(value: Vector2):
+	sprite_scale = value
+	$Sprite2D.scale = sprite_scale
 
 
 func pick_up():
