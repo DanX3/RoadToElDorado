994e38e334205cbee85a251f231e25e4a9b87560
Alessandro <alessandro.mamusa@nextroll.com>
2023-07-23 10:01:04 +0200

    move compass to ui

--- Scripts/compass.gd
+++ Scripts/compass.gd
@@ -1,19 +1,18 @@
-extends Node2D
+extends CanvasLayer
 
 @export var path: Path2D
 @export var min_dist_to_progress: float = 20
 @export var max_dist_to_be_lost: float = 300
+@export var origin: Node2D
 
 var index: int = 0
 
 
-func _process(delta):
-	position = get_global_mouse_position()
-	
+func _process(delta):	
 	var path_point_pos = path.to_global(path.curve.get_point_position(index))
-	var dist = position.distance_to(path_point_pos)
-	look_at(path_point_pos)
+	var dist = origin.global_position.distance_to(path_point_pos)
 	
+	%Arrow.rotation = (path_point_pos - origin.global_position).angle()
 
 	if dist < min_dist_to_progress:
 		index += 1
